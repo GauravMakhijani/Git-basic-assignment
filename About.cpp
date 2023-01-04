802f0cd381546b0e0805a7b495ad7725d27a3ecd
Gaurav Makhijani <makhijani.gaurav19@gmail.com>
2023-01-04 13:19:53 +0530

    added query feature

---
+++ About.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"I am from Bhopal, MP"<<endl;
+    return 0;
+}
--- Introduction.cpp
+++ Introduction.cpp
@@ -3,5 +3,9 @@ using namespace std;
 int main(){
     cout<<"hello!"<<endl;
     cout<<"My name is Gaurav Makijani."<<endl;
+    cout<<"How may I help you?"<<endl;
+    string s;
+    cin>>s;
+    cout<<"okay, please wait!"<<endl;
     return 0;
 }
